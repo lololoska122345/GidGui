3db56c5e1de9b85bac2fb2d6f33e663ebfe6b2ba
lololoshka 122345 <milanatrutneva@gmail.com>
2023-04-22 21:56:53 +0300

    New changes.

--- 3.c
+++ 3.c
@@ -3,14 +3,13 @@
 
 int main(int argc, char **argv)
 {
-    
-    printf("Enter mine name: ");
+    char name[255];
+
+    printf("Enter your name: ");
     fgets(name, 255, stdin);
     printf("length = %d\n", strlen(name)); /* debug line */
     name[strlen(name)-1] = '\0'; /* remove the newline at the end */
 
-    printf("Hello %s!\n", name);
-
-    printf("Do you like cats %s!\n", name %s?\n");
+    printf("I like you, %s!\n" name);
     return 0;
 }
